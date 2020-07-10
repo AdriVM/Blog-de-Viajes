6bde118f1fbbaa694cc155cc5eab9261dd534c6c
Adrián <avazmir1337@gmial.com>
2020-07-10 14:09:53 +0200

    Comienzo del footer - Creación de Widgets

--- footer.php
+++ footer.php
@@ -1,5 +1,17 @@
 <footer>
-
+    <div class="container">
+        <div class="row">
+            <div class="col-8">
+                <div class="row">
+                    <?php dynamic_sidebar( 'footer_widget' ); ?>
+                </div>
+            </div>
+            <div class="col-4">
+                <h3>Redes Sociales</h3>
+            </div>
+        </div><!-- cierre row -->
+    </div><!-- cierre container -->
+    <p class="text-center footer_end">Blog de Viajes &copy; <?php echo date('Y'); ?></p>
 </footer>
 
 <?php wp_footer(); ?>
--- functions.php
+++ functions.php
@@ -8,8 +8,11 @@ function blogviajes_scripts(){
      /* BOOTSTRAP */
      wp_enqueue_script('bootstrapJS', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js', array('jquery'), '4.5.0', true);
 
+     /* FONT AWESOME */
+     wp_enqueue_script('fontawesomeJS', get_template_directory_uri() . '/js/all.min.js', array('jquery'), '5.13.1', true);
+
      //SCRIPTS
-     wp_enqueue_script('scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'));
+     wp_enqueue_script('scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0.0', true);
 }
 add_action('wp_enqueue_scripts', 'blogviajes_scripts');
 
@@ -27,6 +30,9 @@ function blogviajes_styles() {
     wp_enqueue_style('ralewayFont', 'https://fonts.googleapis.com/css2?family=Raleway:wght@900&display=swap', array(),'1.0.0');
     wp_enqueue_style('baskervilleFont', 'https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@700&display=swap', array(), '1.0.0');
 
+    /* FONT AWESOME */
+    wp_enqueue_style('fontawesomeCSS', get_stylesheet_directory_uri() . '/css/all.min.css', array(), '5.13.1');
+
     /* STYLE.CSS */
     wp_enqueue_style('style', get_stylesheet_uri());
 }
@@ -38,6 +44,21 @@ add_action('wp_enqueue_scripts', 'blogviajes_Styles');
 register_nav_menus( array(
     'menu_principal' => __('Menu Principal', 'blogviajes')
 ) );
+
+// WIDGET
+function blogviajes_widgets(){
+    register_sidebar(array(
+        'name' => __('Footer Widgets'),
+        'id' => __('footer_widget'),
+        'description' => 'widgets para el footer',
+        'before_widget' => '<div id="&1$s" class="widget col-6 %2$s">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>'
+
+    ));
+}
+add_action( 'widgets_init', 'blogviajes_widgets' );
 
 //AÑADIMOS SOPORTE DE IMAGEN DESTACADA AK TEMA
 add_theme_support( 'post-thumbnails' );
