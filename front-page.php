627316892a8f14ed8a0d995b1c7f7f259b5bbf57
Adrián <avazmir1337@gmial.com>
2020-07-10 14:09:13 +0200

    Cambios de diseño en front-page

--- front-page.php
+++ front-page.php
@@ -16,14 +16,33 @@
             ?>
             <div class="contenido-entrada card">
                 <h3><?php the_title(); ?></h3>
-                <p>Escrito el: <span><?php the_time(get_option('date_format')); ?></span></p>
-                <p>Por: <span><?php the_author(); ?></span></p>
-                <a href="<?php the_permalink(); ?>" class="btn btn-primary">Leer más</a>
+                <div class="card-body">
+                    <p>Escrito el: <span><?php the_time(get_option('date_format')); ?></span></p>
+                    <p>Por: <span><?php the_author(); ?></span></p>
+                    <a href="<?php the_permalink(); ?>" class="btn btn-primary">Leer más</a>
+                </div>
             </div>
         </div>
         <?php endwhile; wp_reset_postdata();?>
     </div>
 
+</section>
+
+<section class="categorias-blog container">
+    <h2 class="text-center">Entradas por Categoría: </h2>
+    <?php $categorias = get_categories(); ?>
+    <div class="row">
+        <?php foreach($categorias as $categoria): ?>
+        <div class="col-6 col-md-6 col-lg-3 categoria">
+            <a href="<?php echo get_category_link($categoria->cat_ID)?>">
+                <i class="<?php echo $categoria->description; ?>"></i>
+                <div class="clearfix mb-3 categoria_nombre">
+                    <?php echo $categoria->name; ?>
+                </div>
+            </a>
+        </div>
+        <?php endforeach;?>
+    </div>
 </section>
 
 <?php get_footer(); ?>
